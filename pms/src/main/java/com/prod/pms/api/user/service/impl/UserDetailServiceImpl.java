393f4b5ab9ace3826c900c73f7c1e62bafffab78
KimDahun <ekgnsl2002@gmail.com>
2024-09-10 17:31:15 +0900

    2024-09-10-03
    UserInfo 패키지명 수정

--- pms/src/main/java/com/prod/pms/api/user/service/impl/UserDetailServiceImpl.java
+++ pms/src/main/java/com/prod/pms/api/user/service/impl/UserDetailServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class userDetailServiceImpl implements UserDetailsService {
+public class UserDetailServiceImpl implements UserDetailsService {
     private final UserInfoRepository userInfoRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
